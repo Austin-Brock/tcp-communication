5560aca1a740c758c908a7453880c6c977f2175e
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-02-25 22:15:30 -0500

    updated the server file

--- server/server.py
+++ server/server.py
@@ -1,34 +1,28 @@
-import socket
+from socket import *
 import random
 
 # Server setup
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_host = 'localhost'
-server_port = 12345
-server_socket.bind((server_host, server_port))
-server_socket.listen()
-
-print("Server of Bob Smith is listening on port", server_port)
+server_port = 12000
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('localhost', server_port))
+server_socket.listen(1)
+server_name = "Server of Bob Smith"
+print(server_name + " is now ONLINE")
 
 while True:
-    # Accept a connection
-    client_socket, addr = server_socket.accept()
-    print(f"Received a connection from {addr}")
-    
-    # Receive the message from the client
-    message = client_socket.recv(1024).decode()
-    client_name, client_number = message.split(',')
-    client_number = int(client_number)
-    print(f"Client's name: {client_name}")
-    print("Server's name: Server of Bob Smith")
-    
-    # Server generates a random number, calculates the sum
-    server_number = random.randint(1, 100)
-    sum_numbers = server_number + client_number
-    
-    # Send the response to the client
-    response = f"Server of Bob Smith,{server_number},{sum_numbers}"
-    client_socket.send(response.encode())
-    
-    # Close the client connection
-    client_socket.close()
+    connection_socket, addr = server_socket.accept()
+    received_data = connection_socket.recv(1024).decode()
+    client_name, client_num_str = received_data.split(', ')
+    client_num = int(client_num_str)
+
+    # Server's actions
+    server_num = random.randint(1, 100)
+    sum_numbers = client_num + server_num
+    print(f"{client_name} connected. {server_name}")
+
+    # Creating response message
+    response_message = f"{server_name}, Server Number: {server_num}, Sum: {sum_numbers}"
+    connection_socket.send(response_message.encode())
+    print("Sent response to client.")
+    connection_socket.close()
+
